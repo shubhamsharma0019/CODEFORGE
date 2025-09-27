83d5d3ec0545a48c34b6448a5ea1373ffe5f3687
shubham sharma <shubhamsharma639615@gmail.com>
2025-09-27 10:06:01 +0530

    sum of natural numbers

--- Sum_Of_Natural_No.java
+++ Sum_Of_Natural_No.java
@@ -6,10 +6,7 @@ public class Sum_Of_Natural_No {
         System.out.println("Enter the number");
         int n = scn.nextInt();
         
-        int sum = 0;
-        for(int i=1;i<=n;i++){
-           sum = sum +i;
-        }
+        int sum = (n*(n+1))/2;
         System.out.println(sum);
     }
     
